0f06ee739087898a13cd468077303487679a2ca3
Adam Bengtsson <adam.bengtsson@hotmail.com>
2017-05-10 14:08:39 +0200

    Add files via upload

---
+++ Labb2 Del D - 2.py
@@ -0,0 +1,20 @@
+from socket import *
+import time
+serverPort = 80
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('',serverPort))
+count = 0
+
+
+serverSocket.listen(1)
+
+print ('The TCP server is ready to receive')
+connectionSocket, addr = serverSocket.accept()
+while count < 350:
+    sentence = connectionSocket.recv(1024).decode()
+    print (sentence)
+    count = count+1
+    
+
+connectionSocket.close()
